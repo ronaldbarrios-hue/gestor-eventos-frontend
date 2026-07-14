b7fab5887326348b78dc26771eca57d6690e66b2
ronaldbarrios-hue <ronald.barrios@estudiantesunibague.edu.co>
2026-07-14 02:10:19 -0500

    Wrap App component with ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { registerSW } from 'virtual:pwa-register';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 
 /* Registra el service worker generado por vite-plugin-pwa. Se actualiza
    automáticamente en segundo plano (registerType: 'autoUpdate' en vite.config.js). */
@@ -12,6 +13,8 @@ if ('serviceWorker' in navigator) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
